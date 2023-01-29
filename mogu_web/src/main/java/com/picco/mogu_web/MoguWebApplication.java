7a7dac6d73d66cde783de121ec48fcd2367137ce
iamusera <liuzy@kaiyufound.com>
2023-01-29 18:00:34 +0800

    init

---
+++ mogu_web/src/main/java/com/picco/mogu_web/MoguWebApplication.java
@@ -0,0 +1,13 @@
+package com.picco.mogu_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoguWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoguWebApplication.class, args);
+	}
+
+}
---
+++ mogu_web/src/test/java/com/picco/mogu_web/MoguWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picco.mogu_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoguWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
